51ce88f497f545622a68e544640fa25560cf632d
Vitalik <si_vitalik@yahoo.com>
2016-10-18 21:07:49 +0300

    test discounts bday mack

--- test_discounts.py
+++ test_discounts.py
@@ -3,16 +3,14 @@ from datetime import date
 from Cart import Cart
 from User import User
 from Item import Item
+from unittest.mock import Mock
+from discounts import bday_discount
 
 class DiscountsTestCase(unittest.TestCase):
 
     def setUp(self):
         self.user = User('username', 'askgha')
         self.cart = Cart(self.user)
-
-    def tearDown(self):
-        del self.cart
-        del self.user
 
     def test_discount_gt_1000(self):
         self.cart.add(Item('item1', 100), 10)
@@ -27,10 +25,10 @@ class DiscountsTestCase(unittest.TestCase):
 #        self.cart.add(Item('item1', 100))
 #        self.assertEqual(self.cart.discount(), 10)
     def test_bday_discount(self):
-        user = User('username', 'askgha', bday = date.today())
-        cart = Cart(user)
-        cart.add(Item('item1', 100))
-        self.assertEqual(cart.discount(), 10)
+        cart = Mock()
+        cart.user.bday = date.today()
+        cart.total.return_value = 100
+        self.assertEqual(bday_discount(cart), 10)
 
 if __name__ == '__main__':
     unittest.main()
